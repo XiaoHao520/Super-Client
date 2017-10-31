0e86df1d406a032473fb8638c164d42d69cc7402
xiaohao520 <ganxiaohao520@163.com>
2017-10-31 18:00:22 +0800

    做注册页

---
+++ app/src/main/java/com/superschool/activity/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.superschool.activity;
+
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+import com.superschool.R;
+
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class RegisterActivity extends Activity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+    }
+
+
+
+}
+
